24c0bfc000d510c76d3006ebffddc985d1ee9aa8
404Paolo <131349768+404Paolo@users.noreply.github.com>
2024-08-08 15:09:44 +0800

    Add files via upload

--- main_script.js
+++ main_script.js
@@ -2,13 +2,30 @@ var isSelecting = false
 var isSelectingAll = false
 var isAdding = false
 var isEditing = false
+var isConfirming = false
 var dialogOpen = ""
-const selectAllIcon = document.querySelector(".selectall-icon")
-const selectionIcon = document.querySelectorAll(".selection-icon")
-const dialogOptions = document.querySelector(".dialog-options")
-const recordsRow = document.querySelectorAll(".text-editable")
-const confirmButtons = document.querySelector(".confirm-buttons")
-const addUserFields = document.querySelectorAll(".add")
+var userCount = 0
+var medCount = 0
+var inventoryCount = 0
+var transactionCount = 0
+var userRecord_template = ``
+var medRecord_template = ``
+var inventoryRecord_template = ``
+var transactionRecord_template = ``
+var selectAllIcon = document.querySelector(".selectall-icon")
+var selectionIcon = document.querySelectorAll(".selection-icon")
+var dialogOptions = document.querySelector(".dialog-options")
+var usersGroup = document.querySelector(".users-group")
+var medsGroup = document.querySelector(".meds-group")
+var inventoryGroup = document.querySelector(".inventory-group")
+var transactionsGroup = document.querySelector(".transactions-group")
+var usersTitle = document.querySelector("#users")
+var medsTitle = document.querySelector("#meds")
+var inventoryTitle = document.querySelector("#inventory")
+var transactionsTitle = document.querySelector("#transactions")
+var recordsRow = document.querySelectorAll(".text-editable")
+var confirmButtons = document.querySelector(".confirm-buttons")
+var addUserFields = document.querySelectorAll(".add")
 
 function toggleAddMode(){
     if(isEditing){
@@ -72,6 +89,7 @@ function makeHidden(){
 
 function toggleSelectionButton(str){
     const docSamp = document.querySelector(str)
+    console.log(str)
 
     if(docSamp.innerHTML == "○"){
         docSamp.innerHTML = "&#x25CF;"
@@ -166,3 +184,89 @@ function toggleSelectAll(){
         isSelectingAll = false
     }
 }
+
+function changeFrag(str){
+    usersGroup.style.display = "none"
+    medsGroup.style.display = "none"
+    inventoryGroup.style.display = "none"
+    transactionsGroup.style.display = "none"
+    usersTitle.style.borderBottom = "0"
+    medsTitle.style.borderBottom = "0"
+    inventoryTitle.style.borderBottom = "0"
+    transactionsTitle.style.borderBottom = "0"
+
+    const docSamp = document.body.querySelector("."+str+"-group")
+    docSamp.style.display = "grid"
+
+    const docSamp2 = document.body.querySelector("#"+str)
+    docSamp2.style.borderBottom = "3px solid #1436a1"
+}
+
+function init(){
+    for(let i = 0; i < 20; i++){
+                
+        usersGroup.innerHTML += `
+        <div class="records-row selection-icon" id="user-selection${userCount}" onclick="toggleSelectionButton('#user-selection${userCount}')">&cir;</div>
+        <div class="records-row"> <img class="circle-small" src="assets/ic_profpic.webp"></div>
+        <div class="records-row text-editable"> Christian Paolo Reyes</div>
+        <div class="records-row text-editable"> cpaolo852@gmail.com</div>
+        <div class="records-row text-editable"> Fairview</div>
+        <div class="records-row text-editable"> Manager</div>
+        <div class="records-row text-editable"> May 10, 2024</div>
+        <div class="records-row text-editable"> Juan Dela Cruz</div>
+        `
+        
+        medsGroup.innerHTML += `
+        <div class="records-row selection-icon" id="med-selection${medCount}" onclick="toggleSelectionButton('#med-selection${medCount}')">&cir;</div>
+        <div class="records-row"> <img class="circle-small" src="assets/ic_medbox.png"> </div>
+        <div class="records-row">Chlorphenamine Maleate</div>
+        <div class="records-row">Neozep</div>
+        <div class="records-row">50</div>
+        <div class="records-row">365</div>
+        <div class="records-row">A pill box is a container used to organize your medication doses for a certain length of time. A pill box is also called a pill container or pill case.</div>
+        `
+
+        inventoryGroup.innerHTML += `
+        <div class="records-row selection-icon" id="inventory-selection${inventoryCount}" onclick="toggleSelectionButton('#inventory-selection${inventoryCount}')">&cir;</div>
+        <div class="records-row">1</div>
+        <div class="records-row">Chlorphenamine Maleate</div>
+        <div class="records-row">Neozep</div>
+        <div class="records-row">50</div>
+        <div class="records-row">365</div>
+        <div class="records-row">Test</div>
+        <div class="records-row">Test</div>
+        `
+
+        transactionsGroup.innerHTML += `
+        <div class="records-row selection-icon" id="transaction-selection${transactionCount}" onclick="toggleSelectionButton('#transaction-selection${transactionCount}')">&cir;</div>
+        <div class="records-row"> <img class="circle-small" src="assets/ic_profpic.webp"></div>
+        <div class="records-row text-editable"> Christian Paolo Reyes</div>
+        <div class="records-row text-editable"> cpaolo852@gmail.com</div>
+        <div class="records-row text-editable"> Fairview</div>
+        <div class="records-row text-editable"> Manager</div>
+        <div class="records-row text-editable"> May 10, 2024</div>
+        <div class="records-row text-editable"> Juan Dela Cruz</div>
+        `
+
+        userCount += 1
+        medCount += 1
+        inventoryCount += 1
+        transactionCount += 1
+    }
+    
+    selectAllIcon = document.querySelector(".selectall-icon")
+    selectionIcon = document.querySelectorAll(".selection-icon")
+    dialogOptions = document.querySelector(".dialog-options")
+    usersGroup = document.querySelector(".users-group")
+    medsGroup = document.querySelector(".meds-group")
+    inventoryGroup = document.querySelector(".inventory-group")
+    transactionsGroup = document.querySelector(".transactions-group")
+    usersTitle = document.querySelector("#users")
+    medsTitle = document.querySelector("#meds")
+    inventoryTitle = document.querySelector("#inventory")
+    transactionsTitle = document.querySelector("#transactions")
+    recordsRow = document.querySelectorAll(".text-editable")
+    confirmButtons = document.querySelector(".confirm-buttons")
+    addUserFields = document.querySelectorAll(".add")
+}
+init()
